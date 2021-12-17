16b39536c36bf01f33c9ca3d7c4c5189e26c385d
Abelatnafu <Abeljayirv@gmail.com>
2021-12-17 04:58:57 -0800

    Initial commit

---
+++ intro_sliding door.py
@@ -0,0 +1,51 @@
+# Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+# Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+
+Array= [1, 3, 2, 6, -1, 4, 1, 8, 2]
+K=5
+
+def average_subarray(array, k):
+    sum = 0             #the sum for the k amounts in the subarray
+    start = 0           #the starting point
+    end = start + k     #end of the the subarray
+    avg_array = []      #the array of averages that are being returned at the end
+
+    # Loop to get the first  k sum
+    for i in range (start, end):
+        sum += array[i]
+    avg_array.append(sum/k)
+
+    # slide 1 to the left by the subtracting the one before and adding the one after
+    start += 1
+    size = len(array)
+
+    for i in range (start ,size):
+        if start + k <= size:
+            sum -= array[start-1]
+            sum += array[start + k - 1]
+            avg_array.append(sum / k)
+            start += 1
+    return avg_array
+
+
+def avg_subarray(array, k):
+    sum = 0             #the sum for the k amounts in the subarray
+    start = 0           #the starting point
+    end = len(array)     #end of the the subarray
+    avg_array = []      #the array of averages that are being returned at the end
+
+
+    for i in range (start, end):
+        sum += array[i]
+
+        if (i >= k-1):
+            avg_array.append(sum / k)
+            sum-= array[start]
+            start += 1
+    return avg_array
+
+
+
+answer = avg_subarray(Array, K)
+print(answer)
+# print(len(Array))
---
+++ maximum_sum_subarray.py
@@ -0,0 +1,26 @@
+# Given an array of positive numbers and a positive number ‘k’,
+# find the maximum sum of any contiguous subarray of size ‘k’.
+
+Array = [2, 1, 5, 1, 3, 2]
+Array2 = [2, 3, 4, 1, 5]
+p=2
+K = 3
+
+def max_sum_sub(array, k):
+    start = 0
+    highest_sum = 0
+    sum = 0
+    size = len(array)
+
+    for i in range(size):
+        sum += array[i]
+
+        if (i >= k -1):
+            if highest_sum < sum:
+                highest_sum = sum
+            sum -= array[start]
+            start += 1
+    return highest_sum
+
+answer = max_sum_sub(Array2, p)
+print(answer)
